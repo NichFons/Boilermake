f6a8817bbf4a45fa31a87f036b2fe13f2b609bf7
NichFons <nfonsec@purdue.edu>
2014-10-18 22:48:25 -0400

    Contributed by Fixing Spelling Errors

--- boilermake/index.php
+++ boilermake/index.php
@@ -170,7 +170,7 @@ include('connection.php');
 
 <div class="remodal" data-remodal-id="about">
     <h1>About stroll</h1>
-    <p class="left">Allows users to create polls for people in their local area. Need to know what the best restraunt in town is? Make a stroll poll. Have to know where to find the best parties are around your campus? Make a stroll poll. Looking to figure out the opinions of a band in your area? Make a stroll poll.</p>
+    <p class="left">Allows users to create polls for people in their local area. Need to know what the best restaurant in town is? Make a stroll poll. Have to know where to find the best parties are around your campus? Make a stroll poll. Looking to figure out the opinions of a band in your area? Make a stroll poll.</p>
 
 <p class="left">No account is needed to vote or create poll. No usernames. No nonsense. Just go to the app on your laptop, Android, or iOS device and find out what people near you think.</p>
 
