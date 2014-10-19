4272c7bd4789c8596dbe4fe97735ec5697d61b7e
David Teter <david@teter.us>
2014-10-18 20:06:37 -0400

    PHP Output

--- boilermake/location.php
+++ boilermake/location.php
@@ -1,12 +1,12 @@
 <?php
 
 // set cookie for a day
-if (isset($_GET['long'])){
+if (isset($_GET['user_long'])){
     $_SESSION['user_long'] = $_GET['user_long'];
     $_SESSION['user_lat'] = $_GET['user_lat'];
 }
-else{
+else if (!isset($_SESSION['user_long'])){
     $_SESSION['user_long'] = 0;
     $_SESSION['user_lat'] = 0;
 }
-?>
+echo $_SESSION['user_long'] . $_SESSION['user_lat']; ?>
--- boilermake/response.php
+++ boilermake/response.php
@@ -1,1 +1,7 @@
 <!-- Lock users from editing their polls after submitted once -->
+<?php mysql_query("insert into Poll (Question, PollDate, latitude, longitude) values ('Question', now()," . $_SESSION['user_long'] . ", " . $_SESSION['user_lat'] . ");")
+                        or die (mysql_error());
+
+
+insert into Poll (Question, PollDate, latitude, longitude) values ('Question', now(), 1, 1);
+insert into Response (PollID, Response, Count) values (16, 'Test me2', 1+1);
